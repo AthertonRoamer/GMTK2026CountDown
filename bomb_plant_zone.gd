e92ae5aab1552bc5d0766a07c685a8c83a38a85c
AthertonRoamer <Riyria2@protonmail.com>
2026-07-25 11:31:36 -0500

    Added game loop

--- bomb_plant_zone.gd
+++ bomb_plant_zone.gd
@@ -1,1 +1,59 @@
-extends Area2D
+class_name BombPlantZone
+extends InteractableObject
+
+var planting : bool = false
+@export var plant_time : float = 2
+var plant_time_elapsed : float = 0
+var planted : bool = false
+@onready var count_down : CountDown = get_parent().get_node("CountDown")
+
+func _ready() -> void:
+	super()
+	
+	count_down.objective_just_completed.connect(on_objective_completed)
+	visible = false
+	
+	
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("plant") and not planted:
+		planting = true
+		$Sprite2D.visible = true
+		$BombSprite.visible = true
+		$Sprite2D/Timer.start()
+	if Input.is_action_just_released("plant") and not planted:
+		planting = false
+		$BombSprite.visible = false
+	if planting:
+		$Sprite2D.visible = true
+		plant_time_elapsed += delta
+		if plant_time_elapsed >= plant_time:
+			plant()
+			
+			
+func plant() -> void:
+	planted = true
+	planting = false
+	$BombSprite.visible = true
+	active = false
+	$Label.visible = false
+	$StaticBody2D/CollisionShape2D.disabled = false
+	count_down.bomb_planted()
+	
+		
+	
+	
+func on_objective_completed() -> void:
+	active = true
+	visible = true
+
+
+func _on_timer_timeout() -> void:
+	if planted:
+		return
+	if not planting:
+		$Sprite2D.visible = not $Sprite2D.visible
+	if planting:
+		$BombSprite.visible = not $BombSprite.visible
+		
+		
+	
--- hud/hud.gd
+++ hud/hud.gd
@@ -1,16 +1,27 @@
 extends CanvasLayer
 
+@onready var instructions : Control = $Control/Instructions
+@onready var progress_bar : Control = $Control/VBoxContainer/ProgressBar
+@onready var black_screen : Control = $Control/BlackScreen
+@onready var victory_display : Control = $Control/VictoryDisplay
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	get_player().health_changed.connect(on_player_health_changed)
-	$Control/VBoxContainer/ProgressBar.max_value = get_player().starting_health
-	$Control/VBoxContainer/ProgressBar.value = get_player().starting_health
+	progress_bar.max_value = get_player().starting_health
+	progress_bar.value = get_player().starting_health
 	$Control/FailureDisplay.visible = false
+	black_screen.visible = false
+	victory_display.visible = false
 	
 	
-func _process(delta : float) -> void:
-	set_count_down_time(get_parent().get_node("CountDown").time_left)
+func _process(_delta : float) -> void:
+	var count_down : CountDown = get_parent().get_node("CountDown")
+	if get_parent().get_node("CountDown").show_message:
+		$Control/Panel/CountDown.text = get_parent().get_node("CountDown").message
+	else:
+		set_count_down_time(get_parent().get_node("CountDown").time_left)
+	instructions.text = count_down.instruction_text
 
 func on_player_health_changed(h : int) -> void:
 	$Control/VBoxContainer/ProgressBar.value = h 
@@ -23,7 +34,10 @@ func get_player() -> Player:
 func display_failure(msg : String) -> void:
 	$Control/FailureDisplay/VBoxContainer/FailureLabel.text = msg
 	$Control/FailureDisplay.visible = true
+	black_screen.visible = true
 	get_tree().paused = true
+	
+	
 	
 	
 func set_count_down_time(sec : float):
@@ -40,3 +54,13 @@ func set_count_down_time(sec : float):
 
 func _on_restart_pressed() -> void:
 	Main.game.level_manager.reload_active_level()
+	
+	
+func trigger_level_complete() -> void:
+	get_tree().paused = true
+	black_screen.visible = true
+	victory_display.visible = true
+
+
+func _on_next_level_pressed() -> void:
+	Main.game.level_manager.transfer_to_next_level()
